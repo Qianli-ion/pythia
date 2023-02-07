218d5d6a7ef912b1c42ff00e4522ea3c4c2278d6
Qianli Feng <feng.559@osu.edu>
2023-02-07 00:32:55 -0800

    Add parallel processing on language detection

--- code-now/detect_languages.py
+++ code-now/detect_languages.py
@@ -6,6 +6,7 @@ import jsonlines
 import pickle
 from tqdm import tqdm
 import os
+from joblib import Parallel, delayed
 #------------------ Load paper title ---------------------
 # Directory definition
 S2AG_FOLDER = '/home/qianli/Research/literature-discovery/data/S2AG-dataset/2022-12-27/'
@@ -14,9 +15,11 @@ OUTPUT_PKL_DIR = S2AG_FOLDER+'language/'
 
 # Load all jsonl files
 papers_jsonl_list = sorted(glob(PAPERS_FOLDER+'*.jsonl'))
-id_lan_dict = {}    # dictionary saving paper language, {corpusid: language}
-en_num = 0          # total number of English entries
-for this_papers_jsonl in papers_jsonl_list[0:1]:
+
+def detect_language(this_papers_jsonl):
+    id_lan_dict = {}    # dictionary saving paper language, {corpusid: language}
+    en_num = 0          # total number of English entries
+
     print(f'Loading {this_papers_jsonl} ...')
     jsonl_idx = os.path.basename(this_papers_jsonl)[:-6]
     # breakpoint()
@@ -33,11 +36,16 @@ for this_papers_jsonl in papers_jsonl_list[0:1]:
                 en_num += 1
     print(f'Completed. # of English entries = {en_num}')
     print(f'Total {len(id_lan_dict)} entries in dataset. {en_num} English entries found.')
-    
+
     # save the language dictionary as pickle file
     output_pkl_file_dir = f'{OUTPUT_PKL_DIR}{jsonl_idx}.pkl'
     os.makedirs(os.path.dirname(output_pkl_file_dir),exist_ok=True)
     with open(output_pkl_file_dir,'wb') as f:
         pickle.dump(id_lan_dict,f)
     print(f'Output saved at {output_pkl_file_dir}')
+
+# Run language detection in parallel
+Parallel(n_jobs=6)(delayed(detect_language)(this_papers_jsonl) for this_papers_jsonl in papers_jsonl_list[1:])
+
+
 
